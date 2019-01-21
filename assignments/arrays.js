311e2434fadb1684bc766044be1ac60c7b2cc065
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-01-21 17:51:13 -0500

    added arrays challenge 3 with array menthods

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -98,8 +98,12 @@ console.log(lastCar.car_make, lastCar.car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
-console.log();
+
+// typical sort:
+// let carModels = inventory.sort((a, b) => (a.car_model > b.car_model)? 1 : (b.car_model > a.car_model) ? -1 : 0);
+let carModels = inventory.sort((a, b) => a.car_model > b.car_model);
+// let carModels = [];
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
